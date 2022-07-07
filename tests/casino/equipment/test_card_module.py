b426daeb19b004c7d74fe9f19a30ec29c965fbb8
Christopher McCarthy <chris.mccarthy20@protonmail.com>
2022-07-07 20:35:47 +0100

    #8 - Added Shoe class and exceptions module to hold custom Casino-related exceptions. BadCardError exception added. Unit tests for Shoe class added.

--- tests/casino/equipment/test_card_module.py
+++ tests/casino/equipment/test_card_module.py
@@ -1,10 +1,13 @@
-from pycasinosim.casino.equipment.card import Card
+from pycasinosim.casino.equipment.card import Card, Shoe, CardShuffle
 from pycasinosim.casino.equipment.card import Deck
 from pycasinosim.casino.equipment.card import Face
 from pycasinosim.casino.equipment.card import Rank
 
 
 # Card
+from pycasinosim.exceptions import BadCardError
+
+
 def test_face_symbol():
     c = Card(Rank.ACE, Face.HEART)
     assert c.face.symbol == "♥"
@@ -40,3 +43,67 @@ def test_empty_deck():
     while d.can_draw_card:
         d.draw_card()
     assert d.cards_remaining == 0
+
+
+# Shoe
+def test_deck_generation():
+    s = Shoe(6, CardShuffle.DECK_AND_SHOE)
+    assert s.cards_remaining == 312
+
+
+def test_card_replacement():
+    s = Shoe(6, CardShuffle.DECK_AND_SHOE)
+    c = s.draw_card()
+    can_replace = True
+    try:
+        s.replace_overdrawn_card(c)
+    except BadCardError as e:
+        can_replace = False
+    assert can_replace
+
+
+def test_card_repeat_replacement():
+    s = Shoe(6, CardShuffle.DECK_AND_SHOE)
+    c = s.draw_card()
+    try:
+        s.replace_overdrawn_card(c)
+    except BadCardError as e:
+        pass
+    repeat_replacement = False
+    try:
+        s.replace_overdrawn_card(c)
+        repeat_replacement = True
+    except BadCardError as e:
+        pass
+    assert not repeat_replacement
+
+
+def test_replaced_card_then_drawn():
+    s = Shoe(6, CardShuffle.DECK_AND_SHOE)
+    c = s.draw_card()
+    target_uuid = c.uuid
+    try:
+        s.replace_overdrawn_card(c)
+    except BadCardError as e:
+        pass
+
+    assert s.draw_card().uuid == target_uuid
+
+
+def test_bad_card_replacement():
+    s = Shoe(6, CardShuffle.DECK_AND_SHOE)
+    c = s.draw_card()
+    c1 = Card(Rank.ACE, Face.HEART)
+    can_replace = True
+    try:
+        s.replace_overdrawn_card(c1)
+    except BadCardError as e:
+        can_replace = False
+    assert can_replace is False
+
+
+def test_empty_shoe():
+    s = Shoe(6, CardShuffle.DECK_AND_SHOE)
+    for i in range(312):
+        s.draw_card()
+    assert s.cards_remaining == 0
